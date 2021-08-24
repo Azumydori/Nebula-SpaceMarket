2dd15765d9043719fd98daba096e6dd9e0d52553
Yifan <yifanyemontpe@gmail.com>
2021-08-24 08:18:17 +0000

    Added accordion v1

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,120 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime,Enum, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Account(db.Model):
+   __tablename__ = 'account'
+   id = db.Column(db.Integer, primary_key=True)
+   username=db.Column(db.String, unique=True)
+   first_name=db.Column(db.String, nullable=False)
+   last_name=db.Column(db.String, nullable=False)
+   profile_picture = db.Column(db.String, unique=False, nullable=True, default="https://i.ytimg.com/vi/b-CWbJBcgF4/maxresdefault.jpg")
+   country=db.Column(db.String, nullable=False)
+   state=db.Column(db.String, nullable=False)
+   city=db.Column(db.String, nullable=False)
+   address=db.Column(db.String, nullable=False)
+   email = db.Column(db.String, unique=True, nullable=False)
+   _password = db.Column(db.String, nullable=False)
+   _is_active = db.Column(db.Boolean, unique=False, nullable=False, default=True)
+   payment_type = db.Column(db.Enum, unique=False, nullable=False)
+   account_review_id= relationship("Account_Review")
+   whislist_id = relationship("Whislist")
+   order_id = relationship("Order")
+   product_id = relationship("Product")
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+   def __repr__(self):
+      return f'Account {self.name}, mail {self.email}'
+      
+   def to_dict(self):
+      return{
+       "id": self.id,
+        "first_name": self.first_name,
+        "last_name": self.last_name,
+        "email": self.email
+       }
+
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
+      return self
+
+   def validate_password(self,password):
+      is_valid = check_password_hash(self._password,password)
+      print(is_valid)
+      return is_valid
+    
+   def validate_username(self, username):
+      if self.username == username:
+        return True
+      return False
+    
+   @classmethod
+   def get_by_id(cls, id):
+      account = cls.query.filter_by(id=id).one_or_none()
+      return account
+    
+   @classmethod
+   def get_by_username(cls, username):
+      account = cls.query.filter_by(username=username).one_or_none()
+      return account
+    
+
+class Order(db.Model):
+   __tablename__ = 'order'
+   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   order_date=db.Column(db.Date, nullable=False)
+   shipping_date=db.Column(db.Date, nullable=False)
+   shipping_fee=db.Column(db.Numeric, nullable=False)
+   bill_address=db.Column(db.String, nullable=False)
+   is_paid=db.Column(db.Boolean, nullable=False)
+   product_id= db.Column(db.Integer, nullable=False)
+   account_id = Column(db.Integer, ForeignKey('account.id'))
+   line_order_id = relationship("Line_Order")
+
+   
+
+class Whislist(db.Model):
+   __tablename__ = 'whislist'
+   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   account_id = Column(db.Integer, ForeignKey('account.id'))
+   product_id = Column(db.Integer, ForeignKey('product.id'))
+
+
+class Product(db.Model):
+   __tablename__ = 'product'
+   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   product_name=db.Column(db.String, nullable=False)
+   text=db.Column(db.String, nullable=False)
+   media=db.Column(db.String, nullable=False, default="https://i.ytimg.com/vi/b-CWbJBcgF4/maxresdefault.jpg")
+   price=db.Column(db.Numeric, nullable=False)
+   category=db.Column(db.String, nullable=False)
+   sub_category=db.Column(db.String, nullable=False)
+   account_id = Column(db.Integer, ForeignKey('account.id'))
+   line_Order_id = Column(db.Integer, ForeignKey('line_order.id'))
+   whislist_id = relationship("Whislist")
+
+
+
+class Account_Review(db.Model):
+   __tablename__ = 'account_review'
+   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   text=db.Column(db.String, nullable=False)
+   rating=db.Column(db.Integer, nullable=False)
+   account_id = Column(db.Integer, ForeignKey('account.id'))
+
+class Line_Order(db.Model):
+   __tablename__ = 'line_order'
+   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   quantity=db.Column(db.Integer, nullable=False)
+   product_id=db.Column(db.Integer, nullable=False)
+   order_id = Column(db.Integer, ForeignKey('order.id'), nullable=False)
+   product_id = relationship("Product")
+   
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,28 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+
+import ControlledAccordions from "../component/accordion.js";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<ControlledAccordions
+				firstQuestion={"What the hell is a crypto?"}
+				firstAnswer={
+					"A cryptocurrency is a form of digitl asset based on a network distributed accross a large number of computers. This decentralized structure allows them to exist outside the control of governments and central authorities. The word is derived from the encryption techniques which are used to secure the network."
+				}
+				secondQuestion={"What is blockchain?"}
+				secondAnswer={
+					"Blockchain is a specific type of database that stores data in blocks chained together in chronological order. Different types of information can be stored on a blockchain but the most common use so far has been as a ledger for transactions. Decentralized blockchains are immutable, which means that the data entered is irreversible."
+				}
+				thirdQuestion={"How can I pay with crypto?"}
+				thirdAnswer={
+					"You need a digital wallet (like Metamask or Coinomi) which allows you to store, manage, and trade cryptocurrencies. Your public keys/wallet addresses are like your email address, and private keys are similar to the password you use to login to your email. You give out your wallet address so that people can send you coins much like how you provide your email address to your contacts to send you messages."
+				}
+			/>
 		</div>
 	);
 };
