254e015055b53dbd01b8040765457d525ea5086d
Yifan <yifanyemontpe@gmail.com>
2021-08-25 07:37:30 +0000

    Added database

--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum, Numeric
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum, Numeric, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -9,24 +9,32 @@ from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
+PHOTO_URL = "https://i.ytimg.com/vi/b-CWbJBcgF4/maxresdefault.jpg" 
+
+wishlist = Table("Wishlist", db.Model.metadata,
+    Column("account_id", db.Integer, db.ForeignKey("account.id"), primary_key=True),
+    Column("product_id", db.Integer, db.ForeignKey("product.id"), primary_key=True)
+)
+
 class Account(db.Model):
    __tablename__ = 'account'
 
-   id = db.Column(db.Integer, primary_key=True)
+   id = db.Column(db.Integer, autoincrement=True, primary_key=True)
    username=db.Column(db.String, unique=True)
    first_name=db.Column(db.String, nullable=False)
    last_name=db.Column(db.String, nullable=False)
-   profile_picture = db.Column(db.String, unique=False, nullable=True, default="https://i.ytimg.com/vi/b-CWbJBcgF4/maxresdefault.jpg")
-   country=db.Column(db.String, nullable=False)
-   state=db.Column(db.String, nullable=False)
-   city=db.Column(db.String, nullable=False)
-   address=db.Column(db.String, nullable=False)
+   profile_picture = db.Column(db.String, unique=False, default=PHOTO_URL)
+   country=db.Column(db.String)
+   state=db.Column(db.String)
+   city=db.Column(db.String)
+   address=db.Column(db.String)
    email = db.Column(db.String, unique=True, nullable=False)
    _password = db.Column(db.String, nullable=False)
    _is_active = db.Column(db.Boolean, unique=False, nullable=False, default=True)
-   payment_type = db.Column("payment", db.Enum("Fiat", "Paypal", "Crypto", name="payment"), nullable=False)
-   account_review_id= relationship("Account_Review", lazy=True)
-   whislist_id = relationship("Whislist", lazy=True)
+   payment_type = db.Column(db.Enum("Fiat", "Paypal", "Crypto", name="payment"), nullable=False)
+   vendor_review_id= relationship("Account_Review", lazy=True, foreign_keys = 'Account_Review.vendor_id')
+   reviewer_review_id= relationship("Account_Review", lazy=True, foreign_keys = 'Account_Review.reviewer_id')
+   wishlist_id = relationship("Product",lazy=True, secondary=wishlist, backref="accounts")
    order_id = relationship("Order", lazy=True)
    product_id = relationship("Product", lazy=True)
 
@@ -46,52 +54,17 @@ class Account(db.Model):
         "address": self.address,
         "payment_type": self.payment_type
       }
-
-   def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-
-   def update_account_status(self):
-        self._is_active = not _is_active
-        db.session.commit()
-
-   def validate_password(self,password):
-      is_valid = check_password_hash(self._password,password)
-      print(is_valid)
-      return is_valid
-    
-   def validate_username(self, username):
-      if self.username == username:
-        return True
-      return False
-    
-   @classmethod
-   def get_by_id(cls, id):
-      account = cls.query.get(id)
-      return account
-
-   @classmethod
-   def get_by_email(cls, email):
-      account = cls.query.filter_by(email=email).one_or_none()
-      return account
-    
-   @classmethod
-   def get_by_username(cls, username):
-      account = cls.query.filter_by(username=username).one_or_none()
-      return account
     
 
 class Order(db.Model):
    __tablename__ = 'order'
 
-   id = db.Column(db.Integer, nullable=False,primary_key=True)
-   order_date=db.Column(db.Date, nullable=False)
-   shipping_date=db.Column(db.Date, nullable=False)
+   id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+   order_date=db.Column(db.DateTime, nullable=False)
+   shipping_date=db.Column(db.DateTime, nullable=False)
    shipping_fee=db.Column(db.Numeric, nullable=False)
    bill_address=db.Column(db.String, nullable=False)
    is_paid=db.Column(db.Boolean, nullable=False)
-   product_id= db.Column(db.Integer, nullable=False)
    account_id = Column(db.Integer, db.ForeignKey('account.id'))
    line_order_id = relationship("Line_Order", lazy=True)
 
@@ -108,40 +81,19 @@ class Order(db.Model):
       }
 
 
-   def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-
-   
-
-class Whislist(db.Model):
-   __tablename__ = 'whislist'
-
-   id = db.Column(db.Integer, nullable=False,primary_key=True)
-   account_id = Column(db.Integer, db.ForeignKey('account.id'))
-   product_id = Column(db.Integer, db.ForeignKey('product.id'))
-
-
-   def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-
-
 class Product(db.Model):
    __tablename__ = 'product'
 
-   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   id = db.Column(db.Integer,autoincrement=True ,nullable=False,primary_key=True)
    product_name=db.Column(db.String, nullable=False)
    text=db.Column(db.String, nullable=False)
-   media=db.Column(db.String, nullable=False, default="https://i.ytimg.com/vi/b-CWbJBcgF4/maxresdefault.jpg")
+   media=db.Column(db.String, nullable=False, default=PHOTO_URL)
    price=db.Column(db.Numeric, nullable=False)
-   category=db.Column(db.String, nullable=False)
-   sub_category=db.Column(db.String, nullable=False)
-   account_id = Column(db.Integer, db.ForeignKey('account.id'))
-   line_Order_id = Column(db.Integer, db.ForeignKey('line_order.id'))
-   whislist_id = relationship("Whislist", lazy=True)
+   category=db.Column(db.Enum("Clothing", "Computers", "Home & Garden", "Sports", "NFTs", "Cellphones", "Gaming", "Movies, Books & Music", "Appliances", "TV, Audio & Cameras", "Motorbikes", "Other", name = "categories"), nullable=False)
+   account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+   line_order_id = db.Column(db.Integer, db.ForeignKey('line_order.id'))
+   line_order_relation = relationship("Line_Order", lazy = True, foreign_keys = 'line_order.id')
+   wishlist_id = relationship("Account", lazy=True, secondary=wishlist, backref="products")
 
 
    def to_dict(self):
@@ -156,20 +108,14 @@ class Product(db.Model):
       }
 
 
-   def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-
-
-
 class Account_Review(db.Model):
    __tablename__ = 'account_review'
 
-   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   id = db.Column(db.Integer, autoincrement=True, nullable=False,primary_key=True)
    text=db.Column(db.String, nullable=False)
    rating=db.Column(db.Integer, nullable=False)
-   account_id = Column(db.Integer, db.ForeignKey('account.id'))
+   vendor_id = Column(db.Integer, db.ForeignKey('account.id'))
+   reviewer_id = Column(db.Integer, db.ForeignKey('account.id'))
 
 
    def to_dict(self):
@@ -180,16 +126,11 @@ class Account_Review(db.Model):
       }
 
 
-   def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-
 class Line_Order(db.Model):
    __tablename__ = 'line_order'
 
-   id = db.Column(db.Integer, nullable=False,primary_key=True)
+   id = db.Column(db.Integer, autoincrement=True, nullable=False,primary_key=True)
    quantity=db.Column(db.Integer, nullable=False)
+   product_id = Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
    order_id = Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
-   product_id = relationship("Product", lazy=True)
-   
+   line_product_id = relationship("Product", lazy=True, foreign_keys = 'Product.id')
