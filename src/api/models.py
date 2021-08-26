d2ae3c7ce1fe87195624b883200eb0e32b18309e
Yifan <yifanyemontpe@gmail.com>
2021-08-26 15:22:10 +0000

    login v1 not finished

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import aliased
 from sqlalchemy import create_engine
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 
@@ -17,7 +17,7 @@ class Account(db.Model):
 
    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
 
-   username=db.Column(db.String, unique=True)
+   username=db.Column(db.String, unique=True, nullable = False)
    first_name=db.Column(db.String, nullable=False)
    last_name=db.Column(db.String, nullable=False)
    profile_picture = db.Column(db.String, unique=False, default=PHOTO_URL)
@@ -27,10 +27,10 @@ class Account(db.Model):
    address=db.Column(db.String)
    email = db.Column(db.String, unique=True, nullable=False)
    _password = db.Column(db.String, nullable=False)
-   _is_active = db.Column(db.Boolean, unique=False, nullable=False, default=True)
-   payment_type = db.Column(db.Enum("Fiat", "Paypal", "Crypto", name="payment"), nullable=False)
+   _is_active = db.Column(db.Boolean, default=True)
+   payment_type = db.Column(db.Enum("Fiat", "Paypal", "Crypto", name="payment"))
 
-   review_text = db.Column(db.String, nullable = False)
+   review_text = db.Column(db.String)
    reviews_id = db.Column(db.Integer, db.ForeignKey('account.id'))
    have_reviews = db.relationship('Account', backref = db.backref('reviews', remote_side=[id]), lazy='dynamic')
 
@@ -40,7 +40,7 @@ class Account(db.Model):
 
 
    def __repr__(self):
-      return f'Account {self.name}, mail {self.email}'
+      return f'Account name: {self.username}, account id: {self.id}, mail: {self.email}'
       
    def to_dict(self):
       return{
@@ -52,14 +52,47 @@ class Account(db.Model):
         "state": self.state,
         "city": self.city,
         "address": self.address,
-        "payment_type": self.payment_type
+        "payment_type": self.payment_type,
+        "password": self._password
       }
+
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
+
+   def update_account_status(self):
+        self._is_active = not _is_active
+        db.session.commit()
+
+   def validate_password(self,password):
+      is_valid = check_password_hash(self._password,password)
+      return is_valid
+    
+   def validate_username(self, username):
+      if self.username == username:
+        return True
+      return False
+    
+   @classmethod
+   def get_by_id(cls, id):
+      account = cls.query.get(id)
+      return account
+
+   @classmethod
+   def get_by_email(cls, email):
+      account = cls.query.filter_by(email=email).one_or_none()
+      return account
+    
+   @classmethod
+   def get_by_username(cls, username):
+      account = cls.query.filter_by(username=username).one_or_none()
+      return account
 
 
 class Order(db.Model):
    __tablename__ = 'order'
 
-   id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+   id = db.Column(db.Integer, autoincrement=True, primary_key=True)
    order_date=db.Column(db.DateTime, nullable=False)
    shipping_date=db.Column(db.DateTime, nullable=False)
    shipping_fee=db.Column(db.Numeric, nullable=False)
@@ -69,6 +102,9 @@ class Order(db.Model):
    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
    line_order_id = relationship("Line_Order", lazy=True, backref = "order")
 
+
+   def __repr__(self):
+      return f'Order id: {self.id}, order date: {self.order_date}, shipping date: {self.shipping_date}, shipping fee: {self.shipping_fee}, bill address: {self.bill_address}, payment status: {self.is_payed}'
 
    def to_dict(self):
       return{
@@ -81,19 +117,26 @@ class Order(db.Model):
         "product_id": self.product_id,
       }
 
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
 
 class Product(db.Model):
    __tablename__ = 'product'
 
-   id = db.Column(db.Integer,autoincrement=True ,nullable=False,primary_key=True)
+   id = db.Column(db.Integer,autoincrement=True, primary_key=True)
    product_name=db.Column(db.String, nullable=False)
    text=db.Column(db.String, nullable=False)
-   media=db.Column(db.String, nullable=False, default=PHOTO_URL)
+   media=db.Column(db.String, default=PHOTO_URL)
    price=db.Column(db.Numeric, nullable=False)
    category=db.Column(db.Enum("Clothing", "Computers", "Home & Garden", "Sports", "NFTs", "Cellphones", "Gaming", "Movies, Books & Music", "Appliances", "TV, Audio & Cameras", "Motorbikes", "Other", name = "categories"), nullable=False)
 
-   account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable = False)
+   account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
    have_line_order = relationship("Line_Order", lazy = True, backref = "product")
+
+
+   def __repr__(self):
+      return f'Product id: {self.id}, name: {self.product_name}, description: {self.text}, price: {self.price}, category: {self.category}'
 
    def to_dict(self):
       return{
@@ -105,19 +148,39 @@ class Product(db.Model):
         "category": self.category,
       }
 
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
+
 class Wishlist(db.Model):
-    __tablename__ = 'wishlist'
-    from_account = Column(db.ForeignKey('account.id'), primary_key=True)
-    have_product = Column(db.ForeignKey('product.id'), primary_key=True)
-    name = db.Column(db.String, nullable = False)
-    relation_products = relationship("Product", backref = "account_associations")
-    relation_account = relationship("Account", backref = "product_associations")
+   __tablename__ = 'wishlist'
+   from_account = Column(db.ForeignKey('account.id'), primary_key=True)
+   have_product = Column(db.ForeignKey('product.id'), primary_key=True)
+   name = db.Column(db.String, nullable = False)
+   relation_products = relationship("Product", backref = "account_associations")
+   relation_account = relationship("Account", backref = "product_associations")
+
+
+   def __repr__(self):
+      return f'Wishlist name: {self.name}'
+
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
    
 
 class Line_Order(db.Model):
    __tablename__ = 'lineorder'
 
-   id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+   id = db.Column(db.Integer, autoincrement=True, primary_key=True)
    quantity = db.Column(db.Integer, nullable=False)
    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+
+
+   def __repr__(self):
+      return f'Product id: {self.product_id}, quantity: {self.quantity}, order id: {self.order_id}'
+
+   def create(self):
+      db.session.add(self)
+      db.session.commit()
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,21 +1,46 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			baseURL: "/api",
+			currentUser: {}
 		},
+
 		actions: {
+			register: (first_name, last_name, email, password, username) => {
+				fetch(getStore().baseURL.concat("/signup"), {
+					method: "POST",
+					mode: "cors",
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify({ first_name: first_name, last_name: last_name, email: email, password: password, username: username })
+				}).then(resp => {
+					if(!resp.ok){
+						throw Error("Invalid register info");
+					}
+				}).then(responseAsJson => {
+					localStorage.setItem("token", responseAsJson);
+
+				}).catch(error => console.error("There as been an unknown error", error))
+
+			},
+
+			login: (email, password) => {
+				fetch(getStore().baseURL.concat("/login"), {
+					method: "POST",
+					mode: "cors",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ email: email, password: password })
+				}).then(resp => {
+					if(resp.ok) {
+						return resp.json()
+					} else if(resp.status === 401){
+						console.log("Invalid credentials")
+					} else if(resp.status === 400){
+						console.log("Invalid email or password format")
+					} else throw Error("Unknown error")
+				}).then(data => {
+					localStorage.setItem("jwt-token", data.token);
+				}).catch(error => console.error("There as been an unknown error", error))
+
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
