4975d5d417791d4f5891074438d02af9af1ad8fd
Yifan <yifanyemontpe@gmail.com>
2021-08-30 14:22:11 +0000

    Moved faq from home to its own view

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,54 +1,13 @@
 import React, { useContext } from "react";
-import "../../styles/accordion.scss";
-
-import ControlledAccordions from "../component/accordion.js";
+import { Context } from "../store/appContext";
+import "../../styles/home.scss";
 
 export const Home = () => {
-	return (
-		<div className="faq-container">
-			<div className="faq-header">Crypto</div>
-			<ControlledAccordions
-				Question={"What is a crypto?"}
-				Answer={
-					"A cryptocurrency is a form of digitl asset based on a network distributed accross a large number of computers. This decentralized structure allows them to exist outside the control of governments and central authorities. The word is derived from the encryption techniques which are used to secure the network."
-				}
-				PanelId={"q1"}
-			/>
-			<ControlledAccordions
-				Question={"What is blockchain?"}
-				Answer={
-					"Blockchain is a specific type of database that stores data in blocks chained together in chronological order. Different types of information can be stored on a blockchain but the most common use so far has been as a ledger for transactions. Decentralized blockchains are immutable, which means that the data entered is irreversible."
-				}
-				PanelId={"q2"}
-			/>
-			<ControlledAccordions
-				Question={"How can I pay with crypto?"}
-				Answer={
-					"You need a digital wallet (like Metamask or Coinomi) which allows you to store, manage, and trade cryptocurrencies. Your public keys/wallet addresses are like your email address, and private keys are similar to the password you use to login to your email. You give out your wallet address so that people can send you coins much like how you provide your email address to your contacts to send you messages."
-				}
-				PanelId={"q3"}
-			/>
+	const { store, actions } = useContext(Context);
 
-			<div className="faq-header">Account</div>
-			<ControlledAccordions
-				Question={"How to Register?"}
-				Answer={
-					"Go to the landing page or click the user icon to access the register menu.  Enter the email address and password you’ll use for your account. You will receive a verification email in your inbox. "
-				}
-				PanelId={"q4"}
-			/>
-			<ControlledAccordions
-				Question={"How to Disable My Account?"}
-				Answer={"Go to [Account] - [Security] - [Manage Account] - [Disable Account]"}
-				PanelId={"q5"}
-			/>
-			<ControlledAccordions
-				Question={"What is the Referral Program?"}
-				Answer={
-					"By inviting your friends to join the Nebula community. Invite friends and earn crypto together!"
-				}
-				PanelId={"q6"}
-			/>
+	return (
+		<div className="text-center mt-5">
+		
 		</div>
 	);
 };
